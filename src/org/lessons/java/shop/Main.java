fe973298d71073d67b1c4871a5d08f239db4e3f8
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-06 16:04:27 +0100

    add Prodotto class

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,6 +2,16 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
+        Prodotto product = new Prodotto("Scatola", "E' una scatola qualsiasi", 12.00, 22.00);
+
+        System.out.println(product.codice);
+        System.out.println(product.nome);
+        System.out.println(product.nomeCompleto());
+        System.out.println(product.descrizione);
+        System.out.println(product.prezzo);
+        System.out.println(product.iva);
+        System.out.println(product.prezzoBase());
+        System.out.println(product.prezzoPieno());
 
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,4 +1,33 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public double prezzo;
+    public double iva;
+
+    public Prodotto(String nome, String descrizione, double prezzo, double iva) {
+        Random r = new Random();
+        this.codice = r.nextInt(10000);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public double prezzoBase() {
+        return this.prezzo;
+    }
+
+    public double prezzoPieno() {
+        return this.prezzo + (this.prezzo * this.iva / 100);
+    }
+
+    public String nomeCompleto() {
+        return this.codice + "-" + this.nome;
+    }
+
 }
